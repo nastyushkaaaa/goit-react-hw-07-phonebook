e78ac1f06eb5f7bf5364311fee1aed77685cba6c
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-07-08 11:40:14 +0300

    updates

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,38 +1,38 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import * as api from '../services/api';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import * as api from '../services/api';
 
-// export const fetchContacts = createAsyncThunk(
-//   'contacts/getContacts',
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const contacts = await api.fetchContacts();
-//       return contacts;
-//     } catch (error) {
-//       return rejectWithValue(error);
-//     }
-//   }
-// );
+export const fetchContacts = createAsyncThunk(
+  'contacts/getContacts',
+  async (_, { rejectWithValue }) => {
+    try {
+      const contacts = await api.fetchContacts();
+      return contacts;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
-// export const addContact = createAsyncThunk(
-//   'contacts/addContacts',
-//   async (text, { rejectWithValue }) => {
-//     try {
-//       const contact = await api.addContact();
-//       return contact;
-//     } catch (error) {
-//       return rejectWithValue(error);
-//     }
-//   }
-// );
+export const addContact = createAsyncThunk(
+  'contacts/addContacts',
+  async (text, { rejectWithValue }) => {
+    try {
+      const contact = await api.addContact();
+      return contact;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
-// export const deleteContact = createAsyncThunk(
-//   'contacts/deleteContacts',
-//   async (id, { rejectWithValue }) => {
-//     try {
-//       const contact = await api.deleteContact(id);
-//       return contact;
-//     } catch (error) {
-//       return rejectWithValue(error);
-//     }
-//   }
-// );
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContacts',
+  async (id, { rejectWithValue }) => {
+    try {
+      const contact = await api.deleteContact(id);
+      return contact;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
